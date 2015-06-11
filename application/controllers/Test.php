8a4d69fa7f8f48a0ab8df28006ab418121b7c4b4
danishn <danishnadaf@gmail.com>
2015-06-12 00:34:51 +0530

    Member Controller & Model update

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -13,14 +13,13 @@ class Test extends CI_Controller {
         echo "Successfully created Idex Function..";
     }
     
-    public function create($param = 'danish')
+    public function api($param = 'danish')
     {
-        echo "Creating data for $param";
+        echo "Called to Test API with data $param";
     }
     
     public function createMore($param1 = 'danish', $param2 = '25')
     {
-        
         echo "Creating data for $param1 with Qty :: $param2";
         $this->load->view('test');
         $this->load->view('test/test', array('name'=>$param1,'qty'=>$param2));
@@ -30,27 +29,17 @@ class Test extends CI_Controller {
     public function getAll()
     {
         //echo "Getting Data from DB";
-        
-        
-        
         $this->load->model('Test_model');
-        //$this->load->database();
+        $this->auth->validate_request();
         $data = $this->Test_model->getAll();
-        //$data1 = $serializer->serialize($data, 'json');
-        //var_dump($data);
         echo json_encode($data);
     }
         
     public function addUser()
     {
         echo "Adding Data from DB";
-        
-        
-        
         $this->load->model('Test_model');
-        //$this->load->database();
         $data = $this->Test_model->add();
-        
         var_dump($data);
     }
     

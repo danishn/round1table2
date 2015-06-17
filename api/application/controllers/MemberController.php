7eb94cd775da0c1e2c4ad0dd7df7c9a3258eb0ba
danishn <danishnadaf@gmail.com>
2015-06-18 03:35:01 +0530

    New Controller for APIs

--- api/application/controllers/MemberController.php
+++ api/application/controllers/MemberController.php
@@ -1,17 +1,109 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Member extends CI_Controller {
-
-
-    public function login()
-	{
-        $member_email = $this->input->post('email');
-        $member_otp = $this->input->post('otp');
-        
-		$this->load->model('Member_model', 'member');
-        
-        $result = $this->member->login($member_email, $member_otp);
-        echo json_encode($result);
+class MemberController extends CI_Controller {
+    
+    function __construct()
+    {
+		parent::__construct();
+		$this->load->file('application/classes/Response.php'); 
 	}
+
+    /*
+     * URL GET : /api/member/get_all
+    */
+    
+    public function get_members()
+	{ 
+      $response = new Response();
+        
+        if(!$this->auth_service->valid_request)
+        { 
+            $response->setSuccess('false');
+            $response->setdata(null);
+            $response->setError(array(
+                    'code'=>401,
+                    'msg' =>'Access Denied'
+                ));
+            $response->respond();
+            exit;
+        }
+        
+        $this->load->model('Member_model', 'member');
+        
+        $members_list = $this->member->get_all();
+            
+            if(!is_array($members_list) && strpos($members_list, 'error') !== false)
+            {
+                $response->setSuccess('false');
+                $response->setdata(null);
+                $response->setError(array(
+                        'code'=>402,
+                        'msg' =>str_replace('error ', '', $members_list)
+                    ));
+                $response->respond();
+                exit;
+            }else
+            {
+                $response->setSuccess('true');
+                $response->setdata(array('members_list'=>$members_list));
+                $response->setError(null);
+                
+                $response->respond();
+            }
+
+	}
+    
+    /*
+     * URL POST : /api/member/create
+    */
+    
+    public function create_member()
+	{ 
+      $response = new Response();
+        
+        if(!$this->auth_service->valid_request)
+        { 
+            $response->setSuccess('false');
+            $response->setdata(null);
+            $response->setError(array(
+                    'code'=>401,
+                    'msg' =>'Access Denied'
+                ));
+            $response->respond();
+            exit;
+        }
+        
+        $this->load->model('Table_model', 'table');
+        
+        $name = $this->input->post('table_name');
+        $code = $this->input->post('table_code');
+        $desc = $this->input->post('table_desc');
+        $bigUrl = $this->input->post('table_big_url');
+        $thumbUrl = $this->input->post('table_thumb_url');
+        $members = $this->input->post('table_members_count');
+        
+        $table_id = $this->table->add_table($name, $code, $desc, $bigUrl, $thumbUrl, $members);
+            
+            if(!is_int($table_id) && strpos($table_list, 'error') !== false)
+            {
+                $response->setSuccess('false');
+                $response->setdata(null);
+                $response->setError(array(
+                        'code'=>402,
+                        'msg' =>str_replace('error ', '', $table_list)
+                    ));
+                $response->respond();
+                exit;
+            }else
+            {
+                $response->setSuccess('true');
+                $response->setdata(array('table_id'=>$table_id));
+                $response->setError(null);
+                
+                $response->respond();
+            }
+
+	}
+    
 }

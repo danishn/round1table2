61a817b80daaf265faaeef35cc60ecd9add6c6d9
danishn <danishnadaf@gmail.com>
2015-06-25 23:34:13 +0530

    Default Controller changed to TestController

--- api/application/config/routes.php
+++ api/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = "AuthController";
+$route['default_controller'] = "TestController";
 $route['404_override'] = 'AuthController/error404';
 
 $route['test/(:any)'] = "TestController/$1";

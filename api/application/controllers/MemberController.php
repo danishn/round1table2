21a25b490b875f2d139d5c3479b7e9a624dab8d4
danishn <danishnadaf@gmail.com>
2015-07-07 01:56:12 +0530

    Pending Edit_my profile

--- api/application/controllers/MemberController.php
+++ api/application/controllers/MemberController.php
@@ -22,8 +22,8 @@ class MemberController extends CI_Controller {
             $response->respond();
             exit;
         }
-	}
-
+	}    
+    
     /*
      * URL GET : /api/member/get_all
     */
@@ -147,4 +147,81 @@ class MemberController extends CI_Controller {
 
 	}
     
+    
+    /*
+     * URL POST : /api/member/edit_profile
+    */
+    
+    public function edit_member()
+	{ 
+        $response = new Response();
+/*        
+member_id: 14
+table_id: 2
+fname: "Hemant"
+lname: "Gadia"
+gender: "Male"
+mobile: "+977-9851022451"
+email: "jtcl@wlink.com.np"
+blood_group: "A+"
+spouse_name: "Rekha"
+dob: "Aug 25,1967"
+spouse_dob: null
+anniversary_date: "Dec 02,2014"
+image_thumb_url: "/api/public/images/thumb/members/default.jpg"
+image_big_url: "/api/public/images/big/members/default.jpg"
+res_phone: "52552255"
+office_phone: "566565555"
+designation: "-"
+res_city: "-"
+office_city: "-"
+state: ""
+*/        
+        $member_id = $this->input->post('member_id');
+        $table_id = $this->input->post('table_id');
+        $email = $this->input->post('email');
+        $fname = $this->input->post('fname');
+        $lname = $this->input->post('lname');
+        $gender = $this->input->post('gender');
+        $mobile = $this->input->post('mobile');
+        $blood_group = $this->input->post('blood_group');
+        $spouse_name = $this->input->post('spouse_name');
+        $dob = $this->input->post('dob');
+        $spouse_dob = $this->input->post('spouse_dob');
+        $anniversary_date = $this->input->post('anniversary_date');
+        $image_thumb_url = $this->input->post('image_thumb_url');
+        $image_big_url = $this->input->post('image_big_url');
+        $res_phone = $this->input->post('res_phone');
+        $office_phone = $this->input->post('office_phone');
+        $designation = $this->input->post('designation');
+        $res_city = $this->input->post('res_city');
+        $office_city = $this->input->post('office_city');
+        $state = $this->input->post('state');
+        
+        $this->load->model('Member_model', 'member');
+        
+        $members_list = $this->member->get_all();
+            
+            if(!is_array($members_list) && strpos($members_list, 'error') !== false)
+            {
+                $response->setSuccess('false');
+                $response->setdata(null);
+                $response->setError(array(
+                        'code'=>402,
+                        'msg' =>str_replace('error ', '', $members_list)
+                    ));
+                $response->respond();
+                exit;
+            }else
+            {
+                $response->setSuccess('true');
+                $response->setdata(array('members_list'=>$members_list));
+                $response->setError(null);
+                
+                $response->respond();
+            }
+	}
+   
+
+
 }

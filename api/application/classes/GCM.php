6743ee564a78edaa370706e8ea79633e01119cf8
danishn <danishnadaf@gmail.com>
2015-07-11 05:21:38 +0530

    GCM class with new api-key for RTN

--- api/application/classes/GCM.php
+++ api/application/classes/GCM.php
@@ -4,54 +4,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //Reference : http://www.androidhive.info/2012/10/android-push-notifications-using-google-cloud-messaging-gcm-php-and-mysql/
 
 class GCM {
- 
-    public $url;
-    public $headers;
-    public $fields;
-
     
     function __construct() {
-        
-        // Set meta-data for request
-        $this->url = 'https://android.googleapis.com/gcm/send'; 
-        
-        $this->headers = array(
-            'Authorization: key=AIzaSyAdcp_ExRygjsNZCJatSuRa5lq-zVKbaLQ',
-            'Content-Type: application/json'
-        );
-        
-        $this->fields = array(
-            'registration_ids' => null,
-            'notification' => null,
-            'collapse_key' => null
-        );
+
     }
  
     /**
      * Sending Push Notification
-     */
-    public function send_notification($registatoin_ids, $message) {
+    */
+    public function send_notification($registatoin_ids, $message, $data) {
         
-        $this->fields['registration_ids'] = $registatoin_ids;
-        $this->fields['notification'] = $message;
-        $this->fields['collapse_key'] = $message['type'];
- 
-        //echo json_encode($this->fields);exit;
+         $url = 'https://android.googleapis.com/gcm/send'; 
+        
+        // Afif Acc API key for android
+        // 'Authorization: key=AIzaSyAdcp_ExRygjsNZCJatSuRa5lq-zVKbaLQ',
+        
+        $headers = array(
+            'Authorization: key=AIzaSyDA76icBLYgTTwMgmkMQgadlhbyGproojg',
+            'Content-Type: application/json'
+        );
+        
+        $message["icon"] = "@drawable/app_logo";
+        
+        $fields = array(
+            'registration_ids' => $registatoin_ids,
+            'notification' => $message,
+            'data' => $data,
+            'collapse_key' => $data['type']
+        );
+        
+        //echo json_encode($fields);exit;
         
         // Open connection
         $ch = curl_init();
 
         // Set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_URL, $url);
  
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  
         // Disabling SSL Certificate support temporarly
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
  
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
  
         // Execute post
         $result = curl_exec($ch);

70357fa0fc10b643e8a15225c3cd7b7934041f30
danishn <danishnadaf@gmail.com>
2015-07-14 02:14:52 +0530

    view update for Home Page

--- api/application/views/home.php
+++ api/application/views/home.php
@@ -1,10 +1,19 @@
 <html>
 <head>
-        <title>My Test Page</title>
+        <title>RTN Home</title>
 </head>
 <body>
         <h1>Welcome to Home Page!</h1>
-        <h2>Looged in as <?php echo " $user!</h2> <a href='logout'>logout</a>"?>
+        <h2>Logged in as <?php echo " $user"?>!</h2> <a href='logout'>logout</a>
+    
+    <br>
+    <br>
+    
+    <a href='member'>Members</a>
+    <a href='event'>Events</a>
+    <a href='meeting'>Meetings</a>
+    <a href='news'>News</a>
+    <a href='gallery'>Gallery Updates</a>
     
 </body>
 </html>

4aa194de7feca8985bd49e21ead90f9c11e9cd95
danishn <danishnadaf@gmail.com>
2015-07-15 19:43:28 +0530

    Add Member functionality

--- api/application/controllers/admin/MemberController.php
+++ api/application/controllers/admin/MemberController.php
@@ -116,7 +116,133 @@ class MemberController extends CI_Controller {
     
     public function add()
 	{ 
-          
+        if(isset($_POST) && !empty($_POST))
+        {
+            
+            if(!isset($_POST['table_id']) || !isset($_POST['email']) || !isset($_POST['designation']) || !isset($_POST['fname']) || !isset($_POST['lname']) || !isset($_POST['gender']) || !isset($_POST['mobile']) || !isset($_POST['blood_group']) || !isset($_POST['spouse_name']) || !isset($_POST['dob']) || !isset($_POST['spouse_dob']) || !isset($_POST['anniversary_date']) || !isset($_POST['res_phone']) || !isset($_POST['office_phone']) || !isset($_POST['designation']) || !isset($_POST['res_city']) || !isset($_POST['office_city']) || !isset($_POST['state']) || !isset($_POST['status']))
+            {
+                redirect('__admin/member?error=incomplete_data');
+            }
+            
+            $member = new Entities\Members;    
+            $memberInfo = Entities\MembersInfo;
+            
+            if(!$member || !$memberInfo)
+            {
+                redirect('__admin/member?error=add member failed');
+                exit;
+            }
+            
+            $password = '';
+            $client_id = '';
+            $spouse_dob = isset($_POST['spouse_dob']) ? new \DateTime($_POST['spouse_dob']) : null;
+            $spouse_mobile = isset($_POST['spouse_mobile']) ? $_POST['spouse_mobile'] : '-';
+            $res_addr = isset($_POST['res_addr']) ? $_POST['res_addr'] : '-';
+            $office_addr = isset($_POST['office_addr']) ? $_POST['office_addr'] : '-';
+            $fax = isset($_POST['fax']) ? $_POST['fax'] : '-';
+            $website_url = isset($_POST['website_url']) ? $_POST['website_url'] : '-';
+            
+            
+            $member->setTableId($_POST['table_id']);
+            $member->setPassword($password);
+            $member->setRegistrationDate(new \DateTime("now"));
+            $member->setLastVisitDate(new \DateTime("now"));
+            $member->setMemberType(0);
+            $member->setStatus($_POST['status']);
+            $member->setEmail($_POST['email']);
+            $member->setClientId($client_id);       // Need to create this
+            //$member->setOtp($otp);       // Create this on API call
+            $member->setDesignation($_POST['designation']);
+
+            $memberInfo->setFname($_POST['fname']);
+            $memberInfo->setLname($_POST['lname']);
+            $memberInfo->setBigUrl('public/images/big/members/rtn.jpg');
+            $memberInfo->setThumbUrl('public/images/thumb/members/rtn.jpg');
+            $memberInfo->setGender($_POST['gender']);
+            $memberInfo->setDob(new \DateTime($_POST['dob']));
+            $memberInfo->setMobile($_POST['mobile']);
+            $memberInfo->setEmail($_POST['email']);
+            $memberInfo->setRegDate(new \DateTime("now"));
+            $memberInfo->setAnniversaryDate(new \DateTime($_POST['anniversary_date']));
+            $memberInfo->setSpouseName($_POST['spouse_name']);
+            $memberInfo->setSpouseDob(new \DateTime($_POST['spouse_dob']));
+            $memberInfo->setSpouseMobile($spouse_mobile);
+            $memberInfo->setResAddr($res_addr);
+            $memberInfo->setResPhone($_POST['res_phone']);
+            $memberInfo->setResCity($_POST['res_city']);
+            $memberInfo->setOfficeAddr($office_addr);
+            $memberInfo->setOfficePhone($_POST['office_phone']);
+            $memberInfo->setOfficeCity($_POST['office_city']);
+            $memberInfo->setDesignation($_POST['designation']);
+            $memberInfo->setFax($fax);
+            $memberInfo->setWebsiteUrl($website_url);
+            $memberInfo->setOtherDetails($other_details);
+            $memberInfo->setState($_POST['state']);
+            $memberInfo->setState($country);
+            $memberInfo->setState($zip);
+            $memberInfo->setBloodGroup($_POST['blood_group']);
+            $memberInfo->setBusinessAreas($business_areas);
+            
+            
+
+            var_dump($member);echo"<br><br><br>";
+            var_dump($memberInfo);exit;
+            
+             try
+            {
+                $this->em->persist();
+                $this->em->flush();
+                //$temp = $this->get_members_details($member, $memberInfo);
+                //return $temp;
+
+            }catch(Exception $e)
+            {
+                return 'error '. $e->getMessage();
+            }
+
+            if(isset($_FILES['profile_image']))
+            {  
+                $pic = explode('.', $_FILES['profile_image']['name']);
+               // $_FILES['profile_image']['name'] = 'member_'.$member_id.'_profile.'.$pic[count($pic)-1];
+
+                // upload event photo to server & set image URLs
+                $config['upload_path'] = 'public/images/big/members';
+                $config['allowed_types'] = 'gif|jpeg|jpg|png';
+                $config['max_size']	= '10000';
+                $config['overwrite'] = true;
+                $config['file_name'] = 'member_'.$member_id.'_profile.'.$pic[count($pic)-1];
+
+                //$config['file_name']	= 'member_'.$member_id.'.'.$pic[count($pic)-1];
+                //echo 'member_'.$member_id.'_pic.'.$pic[count($pic)-1];exit;
+
+                $this->load->library('upload', $config);
+                $this->upload->initialize($config);
+
+                if ( ! $this->upload->do_upload('profile_image')) {
+                    return 'error '.$this->upload->display_errors();
+                    exit;
+                } else {
+                    $data = $this->upload->data();
+                    $thumb_url  = $big_url = '/api/public/images/big/members/' . $data['file_name'];
+
+
+                    $image = new ImageResize('public/images/big/members/'.$data['file_name']);
+                    $image->scale(20);
+                    $image->save('public/images/thumb/members/'.$data['file_name']);
+                    $thumb_url = '/api/public/images/thumb/members/' . $data['file_name'];
+
+                    $memberInfo->setBigUrl($big_url);
+                    $memberInfo->setThumbUrl($thumb_url);
+                }
+
+            }
+
+           
+        }else
+        {
+            return 'error method not allowed';
+        }
+        
 	}
     
     /*
